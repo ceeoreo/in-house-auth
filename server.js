16566f6c8b83a043bf5248c082f341625cbb1212
ceeoreo <ceoraford@gmail.com>
2024-08-05 11:17:23 -0400

    Add database integration. Not worrking currently

--- server.js
+++ server.js
@@ -7,7 +7,26 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false}));
 
 const PORT = process.env.PORT || 3000;
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'users'
+})
 
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    db.query(`SHOW DATABASES`, (err, result) => {
+      if (err) {
+        console.log(`This is the error: ${err}`);
+      } else {
+        console.log(`This is the result: ${result}`)
+      }
+    })
+  }
+})
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 });
